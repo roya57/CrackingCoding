70feafce47c9a7a7b19e0b46c3551773ecbf7b0e
roya57 <roya@royaa.net>
2015-08-11 21:55:38 -0700

    updated code

--- src/Chapter1/c1_1.java
+++ src/Chapter1/c1_1.java
@@ -1,5 +1,24 @@
 package Chapter1;
+/**
+ * Implement an algorithm to determine if a string has all unique characters. 
+ */
 
 public class c1_1 {
+	private static boolean ifUnique(String s){
+		//ASCII
+		boolean[] chars=new boolean[256];
+		for (int i=0;i<s.length();i++){
+			int c=s.charAt(i);
+			if (chars[c]) return false;
+			else{
+				chars[c]=true;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args){
+		System.out.println(ifUnique("thefoxjfump"));
+		
+	}
 
 }
---
+++ src/Chapter1/c1_1v2.java
@@ -0,0 +1,29 @@
+package Chapter1;
+
+/**
+ * Implement an algorithm to determine if a string has all unique characters. 
+ * What if you can not use additional data structures?
+ */
+
+
+public class c1_1v2 {
+	private static boolean ifUnique(String s){
+		int bitRepresentation=0;
+		for (int i=0;i<s.length();i++){
+			//Assuming characters between 'a' to 'z'
+			int c=s.charAt(i)-'a';
+			if (((1<<c)&bitRepresentation)!=0){
+				return false;
+			}else{
+				bitRepresentation=bitRepresentation|(1<<c);
+				
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args){
+		System.out.println(ifUnique("thefoxjump"));
+		
+	}
+
+}
