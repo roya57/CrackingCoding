03314a2eeffb122a4d9e335772e42fd6f94de86b
roya57 <roya@royaa.net>
2015-08-26 10:34:38 -0700

    initial code

---
+++ src/Chapter1/c1_4.java
@@ -0,0 +1,29 @@
+package Chapter1;
+
+/* Write a method to decide if two strings are anagrams or not. */
+
+public class c1_4 {
+	public static boolean anagram(String s1, String s2){
+		if (s1.length()!=s2.length()){
+			return false;
+		}
+		int[] letters=new int[256];
+		for (int i=0; i<s1.length();i++){
+			int c=(int)s1.charAt(i);
+			letters[c]++;
+		}
+		for (int i=0;i<s2.length();i++){
+			int c=(int)s2.charAt(i);
+			if (letters[c]==0){
+				return false;
+			}else{
+				letters[c]--;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args){
+		System.out.println(anagram("hellooo","hhloeab"));
+	}
+
+}
